b29b85c02436dd2aa8c5f208bcc54f93b97e711b
Adityakataria18 <79639669+Adityakataria18@users.noreply.github.com>
2021-04-19 19:26:05 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,31 @@
+class Paper
+{
+	constructor(x,y){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:0.2
+        }
+    }
+        this.width=50
+        this.height=50
+		this.Body=Bodies.rectangle(x,y,50,50,options)
+		this.image=loadImage("paper.png")
+		World.add(world, ssthis.Body)
+	
+	display()
+    var posX=this.Body.position.x;
+    var posY=this.Body.position.y;
+
+    push()
+    translate(posX, posY);
+    rectMode(CENTER)
+    
+    
+    pop()
+	
+			
+			
+	
+
